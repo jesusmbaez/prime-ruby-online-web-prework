e43bde72ccc5332b8b8d6df8d6a944bd37898875
Jesus Manny Baez <jesusmbaez@gmail.com>
2018-11-19 05:08:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,3 +8,13 @@ def prime?(n)
    end
   end
 end
+
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
+end
